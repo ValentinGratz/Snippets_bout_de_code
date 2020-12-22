f684dd07adae3dbc076fbcdbbfee10c6815e355f
Valentin Grtz <39189263+ValentinGratz@users.noreply.github.com>
2020-12-22 20:14:00 +0100

    Create config.php

---
+++ PHP/function_connexion_bdd/config/config.php
@@ -0,0 +1,10 @@
+// Puis appeler la fonction comme ceci : getDb() ; dans le fichier destinataire. 
+//	Ne pas oublier d’appeler dans config/config.php cela :
+
+<?php
+
+define('DBHOST', 'localhost');
+define('DBPORT', 3306);
+define('DBNAME', 'products');
+define('DBUSER', 'root');
+define('DBPWD', '');
